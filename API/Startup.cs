5df87e08db22b1bd4dc83ff466c026850534bf43
reenamittal87 <reenamittal87@gmail.com>
2021-06-08 21:59:38 +0530

    End of section 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -20,6 +20,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using API.Extensions;
+using API.Middleware;
 
 namespace API
 {
@@ -61,13 +62,14 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            /* if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+            } */
 
+            app.UseMiddleware<ExceptionMiddleware>();
             app.UseHttpsRedirection();
 
             app.UseRouting();
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -28,9 +28,6 @@ export class NavComponent implements OnInit {
       this.router.navigateByUrl('/members');
       //console.log(response);
       //this.loggedIn = true;
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
     });
   }
 
