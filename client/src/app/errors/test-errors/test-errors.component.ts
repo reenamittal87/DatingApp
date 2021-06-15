12800582f741f5ee41a32f5ba8dc9f820cec6eb9
reenamittal87 <reenamittal87@gmail.com>
2021-06-15 23:13:36 +0530

    changing from https to http for angular app.

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-errors.component.css']
 })
 export class TestErrorsComponent implements OnInit {
-  //baseUrl="https://localhost:5001/api/";
-  baseUrl="http://localhost:5001/api/";
+  baseUrl="https://localhost:5001/api/";
+  //baseUrl="http://localhost:5001/api/";
   validationErrors: string[] = [];
 
   constructor(private http: HttpClient) { }
