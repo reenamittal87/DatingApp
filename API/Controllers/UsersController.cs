ef6a09bb7abc219521705f873919fb8014d99bf8
reenamittal87 <reenamittal87@gmail.com>
2021-06-15 22:15:28 +0530

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using API.Interfaces;
 using API.DTOs;
 using AutoMapper;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -52,6 +53,18 @@ namespace API.Controllers
         //var user = await _userRepository.GetUserByUsernameAsync(username);
         //return _mapper.Map<MemberDto>(user);
         return await _userRepository.GetMemberAsync(username);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto){
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        _mapper.Map(memberUpdateDto, user);
+        _userRepository.Update(user);
+
+        if(await _userRepository.SaveAllAsync()) return NoContent();
+        return BadRequest("Failed to update user");
     }
 
     }
--- API/Startup.cs
+++ API/Startup.cs
@@ -79,8 +79,8 @@ namespace API
                 //"sslCert": "./ssl/server.crt",
                 //"ssl":true
             //},
-            //app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            //app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthentication();
             app.UseAuthorization();
